b63b014ca37ac4991e382679e2300e9b99543657
Ruhollah <ruhollahh01@gmail.com>
2024-05-14 00:12:52 +0330

    handle http request target

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -11,19 +12,46 @@ func main() {
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
+		fmt.Println("failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	con, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	for {
+		con, err := l.Accept()
+		if err != nil {
+			fmt.Println("error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
 
-	_, err = con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	if err != nil {
-		fmt.Println("Error writing response: ", err.Error())
-		os.Exit(1)
+		buffer := make([]byte, 1024)
+		n, err := con.Read(buffer)
+		if err != nil {
+			fmt.Println("error reading response: ", err.Error())
+			os.Exit(1)
+		}
+
+		requestContent := string(buffer[:n])
+		requestLines := strings.Split(requestContent, "\r\n")
+		route := strings.Split(requestLines[0], " ")[1]
+
+		var response string
+		switch route {
+		case "/":
+			response = "HTTP/1.1 200 OK\r\n\r\n"
+		default:
+			response = "HTTP/1.1 404 Not Found\r\n\r\n"
+		}
+
+		_, err = con.Write([]byte(response))
+		if err != nil {
+			fmt.Println("error writing response: ", err.Error())
+			os.Exit(1)
+		}
+
+		err = con.Close()
+		if err != nil {
+			fmt.Println("error closing connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 }
